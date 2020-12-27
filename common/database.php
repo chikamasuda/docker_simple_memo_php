c57c499aca78e2618078f8e9ff754845b69d103f
chikamasuda <c.m.062820@gmail.com>
2020-12-27 22:53:14 +0900

    ユーザー登録機能を実装

--- common/database.php
+++ common/database.php
@@ -7,7 +7,7 @@ function getDatabaseConnection() {
   try{
     $database_handler = new PDO('mysql:host=db;daname=simple_memo;charset=utf8mb4', 'root', 'password');
   }
-  catch(PDOExeption $e)
+  catch(PDOException $e)
   {
     echo "DB接続に失敗しました。 <br/>";
     echo $e->getMessage();
